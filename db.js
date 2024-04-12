070867d387e5d7a2b8e80dac3bf8efa7120e2fca
Joeltabe <joeltabe3@gmail.com>
2024-04-12 13:00:51 +0100

    first commit3

--- db.js
+++ db.js
@@ -5,12 +5,15 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGODB_URI="mongodb+srv://chunke135:9lJvdebg5WPs0F4f@cluster0.mbjrcw6.mongodb.net/Quizzy", {
+      useNewUrlParser: true,
+    });
     console.log('MongoDB connected successfully!');
   } catch (err) {
     console.error('Error connecting to MongoDB:', err.message);
     process.exit(1); // Exit on connection failure
   }
 };
+
 
 module.exports = connectDB;
